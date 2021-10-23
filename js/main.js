6794cbd3dff4a18f889ac1df7086058542fba06f
Vando <vandodosreis2001@gmail.com>
2021-10-23 19:31:04 -0300

    transicao do button melhorada

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 let button_area = document.querySelector(".button-area");
 let html = document.querySelector("html");
 let span = document.querySelector(".area-mode span");
-let button = document.querySelector(".button-area .button");
+let button = document.querySelector(".area-mode .button-area .button");
 button_area.addEventListener('click', () => {
   if(html.classList.contains('dark')) {
     html.classList.remove('dark');
     html.classList.add('light');
     span.textContent = "Light Mode";
-    button_area.style.justifyContent = 'flex-end';
-    button_area.style.transitionDuration = "1s";
+    button.style.transform = 'translate(22px, 0)';
   } else {
     html.classList.remove('light');
     html.classList.add('dark');
     span.textContent = "Dark Mode";
-    button_area.style.justifyContent = 'flex-start';
-    button_area.style.transitionDuration = "1s";
+    button.style.transform = 'translate(0px, 0)';
   }
 });
